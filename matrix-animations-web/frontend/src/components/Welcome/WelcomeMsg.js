ad489608f5e731afa6e03bdd0a6aa0517dd1b53d
c-grigsby <grigsby.c@outlook.com>
2023-05-03 14:00:52 -0400

    feat(webApp): add upload file constraint

--- matrix-animations-web/frontend/src/components/Welcome/WelcomeMsg.js
+++ matrix-animations-web/frontend/src/components/Welcome/WelcomeMsg.js
@@ -10,8 +10,8 @@ const WelcomeMsg = () => {
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className={classes.welcome_header}>Matrix Timestamp Animations</h1>
       <p className={classes.welcome_text}>
-        This application will accept a .raw file representing a series of matrix timestamps  <br/>
-        and transform them into a .mp4 file
+        This application will accept a .raw file representing a series of matrix timestamps
+        and transform them into a .mp4 animation file. For best results from the server (hosted on a free tier), keep <text className={classes.welcome_text_conditions}>.raw file upload size below 25MB</text> and <text className={classes.welcome_text_conditions}>timestamp iterations below 300</text>.
       </p>
     </Fragment>
   );
